72689706d0de22614a01f1a89d8d09d899201887
evgen <evgst.gl@gmail.com>
2024-10-25 11:41:22 +0700

    breakpoints added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '374px',
+        'sm': '576px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1366px',
+        '3xl': '1920px'
+      }
+    },
   },
   plugins: [],
 }
