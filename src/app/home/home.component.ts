72ea27f85e64f37914c4bb7bc3bd629d8ba8d829
evgen <evgst.gl@gmail.com>
2024-11-01 06:52:46 +0700

    custom tooltip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,18 @@ import {HeaderComponent} from "../components/header/header.component";
 import {MatDialogContainer} from "@angular/material/dialog";
 import {MatExpansionPanelTitle} from "@angular/material/expansion";
 import {MatToolbar} from "@angular/material/toolbar";
+import {PageLayoutComponent} from "../components/page-layout/page-layout.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    MatDialogContainer,
-    MatExpansionPanelTitle,
-    MatToolbar
-  ],
+    imports: [
+        HeaderComponent,
+        MatDialogContainer,
+        MatExpansionPanelTitle,
+        MatToolbar,
+        PageLayoutComponent
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
